07bd6d7c87fc18294ba6d8f1b2c7c8feb4801010
jedaro <jdanielrodriguezo@gmail.com>
2025-12-17 00:22:23 +0100

    add impl service in controller

--- src/main/java/com/inditex/products/controller/ProductController.java
+++ src/main/java/com/inditex/products/controller/ProductController.java
@@ -1,33 +1,30 @@
 package com.inditex.products.controller;
 
-import com.inditex.products.controller.dtos.ProductDetailDTO;
+import com.inditex.products.exception.ProductNotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.inditex.products.controller.dtos.SimilarProducts;
+import com.inditex.products.model.SimilarProducts;
 import com.inditex.products.service.IProductService;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Slf4j
 public class ProductController {
 
-    private IProductService iProductService;
-    //private ProductMapper mapper;
+    private final IProductService iProductService;
 
     @Operation(
             summary = "Get similar products",
@@ -45,7 +42,8 @@ public class ProductController {
                             responseCode = "404",
                             description = "Product Not found",
                             content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ProductNotFoundException.class)
                             )
                     )
 
@@ -57,15 +55,8 @@ public class ProductController {
     public ResponseEntity<SimilarProducts> getSimilarProduct(@PathVariable(value = "productId", required = true) String productId){
 
         log.info("Calling controller: {}", this.getClass().getName());
-        SimilarProducts responseDTO = SimilarProducts.builder()
-                .similarProducts(List.of(ProductDetailDTO.builder()
-                        .id("4")
-                        .name("Boots")
-                        .price(39.99)
-                        .availability(Boolean.TRUE)
-                        .build()))
-                .build();
 
+        SimilarProducts responseDTO = SimilarProducts.builder().similarProducts(iProductService.getSimilarProducts(productId)).build();
         return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
     }
 
--- src/main/java/com/inditex/products/service/IProductService.java
+++ src/main/java/com/inditex/products/service/IProductService.java
@@ -1,5 +1,15 @@
 package com.inditex.products.service;
 
+import com.inditex.products.model.ProductDetail;
+
+import java.util.List;
+
 public interface IProductService {
 
+    /**
+     * Retrieves list similar products
+     * @param productId product id
+     * @return List<ProductDetail>
+     */
+    public List<ProductDetail> getSimilarProducts(String productId);
 }
